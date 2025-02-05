cff294c177808715ffbaca1cf7b43d9fff9a534b
ashish0082018 <av0082018@gmail.com>
2025-02-06 01:16:20 +0530

    bug fix11

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,45 @@
 import Body from "@/components/home/Body";
 import Footer from "@/components/home/Footer";
 import Navbar from "@/components/home/Navbar";
+import Loadingpage from "@/components/loaders/Loadingpage";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 
 
 
 export default  function Home() {
+  const [isLoading, setIsLoading] = useState(true); 
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);  
+    }, 100);
+
+    return () => clearTimeout(timer); 
+  }, []); //
   return (
     <>
-    <div className="min-h-screen relative"> 
-    <ToastContainer/>
-      <div className="min-h-screen  flex flex-col gap-2 ">
-      <div className="bg-zinc-950 h-12 ">
-      <Navbar/>
-     </div>
-      
-        {/* navbar */}
-
-        {/* body */}
-        <div className="min-h-screen">
-          <Body />
+    {
+      isLoading ? <> <Loadingpage/> </>: <div className="min-h-screen relative"> 
+      <ToastContainer/>
+        <div className="min-h-screen  flex flex-col gap-2 ">
+        <div className="bg-zinc-950 h-12 ">
+        <Navbar/>
+       </div>
+        
+          {/* navbar */}
+  
+          {/* body */}
+          <div className="min-h-screen">
+            <Body />
+          </div>
+          {/*footer  */}
+          <div className="absolute bottom-0 w-full" >
+            <Footer />
+          </div>
         </div>
-        {/*footer  */}
-        <div className="absolute bottom-0 w-full" >
-          <Footer />
         </div>
-      </div>
-      </div>
+    }
 
     </>
   );
