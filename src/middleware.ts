c45c9bcbd3acb1ebdd0369b79e1adbafa97d053e
ashish0082018 <av0082018@gmail.com>
2025-02-05 19:18:32 +0530

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 // Define public routes that do not require authentication
-// eslint-disable-next-line
-const publicRoutes = ["/about", "/contact", "/","/signup","/signin","/verifyotp"];
+
 
 // Define protected routes that require authentication
 const protectedRoutes = ["/dashboard", "/dashboard/style1", "/dashboard/style1/edit1", "/profiles"];
